0dd79895762098b41670894cbe785ba052d300ae
AKHIL RAJ SRIVASTAVA <84132589+AKHILRAJSRIVASTAVA@users.noreply.github.com>
2022-01-07 20:40:55 +0530

    rest-api

---
+++ controllers/auth/registerController.js
@@ -0,0 +1,16 @@
+const registerController = {
+    register(req,res,next){
+        //logic
+        //checklist
+        //validate the request
+        //authorize the request
+        //check if user is in database already
+        //prepare model
+        //store in database
+        //generate jwt token
+        //send response
+        res.json({msg: "hello from server"});
+    }
+}
+
+export default registerController;
